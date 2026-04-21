64e11986732a227a40ef80b31079e189cf7bf487
Maxim Petrukha <mppamaksnm2018@gmail.com>
2026-04-21 16:29:33 +0800

    Initial Commit

---
+++ GestureApp/GestureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureAppApp.swift
+//  GestureApp
+//
+//  Created by Maxim Petrukha on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GestureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
